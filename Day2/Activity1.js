932e6530973c42136b06a6dd10ad0cc841bf5062
Dhruv Gala <galadhruv1611@gmail.com>
2024-07-18 22:08:54 +0530

    did activity 1 for day 2

---
+++ Day2/Activity1.js
@@ -0,0 +1,28 @@
+// # Activity 1 : Arithmetic operators
+//  - Task 1: WAP to add 2 nos and log the result
+//  - Task 2: WAP to subtract 2 nos and log the result
+//  - Task 3: WAP to multiply 2 nos and log the result
+//  - Task 4: WAP to divide 2 nos and log the result
+//  - Task 5: WAP to find remainder of 2 nos when divided and log the result
+
+// Task 1
+let num1 = 10;
+let num2 = 20;
+let sum = num1 + num2;
+console.log("Sum of " + num1 + " and " + num2 + " is " + sum);
+
+// Task 2
+let sub = num1 - num2;
+console.log("Subtraction of " + num1 + " and " + num2 + " is " + sub);
+
+// Task 3
+let mul = num1 * num2;
+console.log("Multiplication of " + num1 + " and " + num2 + " is " + mul);
+
+// Task 4
+let div = num1 / num2;
+console.log("Division of " + num1 + " and " + num2 + " is " + div);
+
+// Task 5
+let rem = num1 % num2;
+console.log("Remainder of " + num1 + " and " + num2 + " is " + rem);
---
+++ Day2/Activity2.js
@@ -0,0 +1,3 @@
+// # Activity 2 : Assignment operators
+//   - Task 6: Use the += operator to add a number to a variable and log the result
+//   - Task 7: Use the -= operator to subtract a number to a variable and log the result
