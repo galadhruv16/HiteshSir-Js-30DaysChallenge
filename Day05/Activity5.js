a3025da65690640e9ac31a8660698d6c171ff76f
Dhruv Gala <galadhruv1611@gmail.com>
2024-07-22 15:34:35 +0530

    day 5

--- Day05/Activity5.js
+++ Day05/Activity5.js
@@ -5,16 +5,22 @@
 // Task 9
 
 function callFunctionMultipleTimes(func, number) {
-    for (let i = 0; i < number; i++) {
-        func();
-    }
+  for (let i = 0; i < number; i++) {
+    func();
+  }
 }
 callFunctionMultipleTimes(() => console.log("Hello"), 5);
 
 // Task 10
 
 function applyFunctions(func1, func2, value) {
-    return func2(func1(value));
+  return func2(func1(value));
 }
 
-console.log(applyFunctions((x) => x + 1, (x) => x * 2, 3)); 
+console.log(
+  applyFunctions(
+    (x) => x + 1,
+    (x) => x * 2,
+    3
+  )
+);
